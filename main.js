b4f1572c1cf98a7dc6a96c01bbc21fc0c0308ae1
techhead55 <techhead55@gmail.com>
2013-07-05 15:30:28 +1000

    v0.0.4.5
    Initial work for layers and shape drawing

--- main.js
+++ main.js
@@ -1,7 +1,16 @@
 ﻿var canvas = document.getElementById('canvas');
 var context = canvas.getContext('2d');
+var canvas1 = document.getElementById('canvas1');
+var context1 = canvas1.getContext('2d');
 var canvas2 = document.getElementById('canvas2');
-var context2 = canvas2.getContext('2d');
+var context2 = canvas1.getContext('2d');
+var canvas3 = document.getElementById('canvas3');
+var context3 = canvas1.getContext('2d');
+var canvas4 = document.getElementById('canvas4');
+var context4 = canvas1.getContext('2d');
+var canvas5 = document.getElementById('canvas5');
+var context5 = canvas1.getContext('2d');
+
 var posit = [];
 var brushColour = "73FF40";
 var brushSize = "10";
@@ -15,7 +24,11 @@ var drawType = "fill";
 var types = ["fill", "grad"];
 var brushGrad;
 var shape = "circle";
-var shapes = ["circle","square"];
+var shapes = ["circle", "square"];
+var layerSelected = 1;
+function setLayer(input) {
+    layerSelected = input;
+}
 function setShape(input) {
     shape = input;
 }
@@ -32,39 +45,39 @@ function setShape(input) {
     shape = input;
 }
 function draw(x, y) {
-    if (y > canvas.height || x > canvas.width) { }
+    if (y > canvas1.height || x > canvas1.width) { }
     else if (drawType === "fill") {
         if (shape === "circle") {
-            context.beginPath();
-            context.arc(x, y, brushSize, 0, 2 * Math.PI, false);
-            context.fillStyle = brushColour;
-            context.fill();
+            context1.beginPath();
+            context1.arc(x, y, brushSize, 0, 2 * Math.PI, false);
+            context1.fillStyle = brushColour;
+            context1.fill();
         }
         else {
-            context.beginPath();
-            context.rect(x - brushSize, y - brushSize, brushSize * 2, brushSize * 2);
-            context.fillStyle = brushColour;
-            context.fill();
+            context1.beginPath();
+            context1.rect(x - brushSize, y - brushSize, brushSize * 2, brushSize * 2);
+            context1.fillStyle = brushColour;
+            context1.fill();
         }
     }
     else {
         if (shape === "circle") {
-            context.beginPath();
-            context.arc(x, y, brushSize, 0, 2 * Math.PI, false);
-            var grd = context.createRadialGradient(x, y, brushSize / brushGrad, x, y, brushSize);
+            context1.beginPath();
+            context1.arc(x, y, brushSize, 0, 2 * Math.PI, false);
+            var grd = context1.createRadialGradient(x, y, brushSize / brushGrad, x, y, brushSize);
             grd.addColorStop(0, brushColour);
             grd.addColorStop(1, 'rgba(255,255,255,0)');
-            context.fillStyle = grd;
-            context.fill();
+            context1.fillStyle = grd;
+            context1.fill();
         }
         else {
-            context.beginPath();
-            context.rect(x - brushSize, y - brushSize, brushSize*2, brushSize*2);
-            var grd = context.createRadialGradient(x, y, brushSize*2/ brushGrad,x-brushSize,y-brushSize,brushSize*2);
+            context1.beginPath();
+            context1.rect(x - brushSize, y - brushSize, brushSize*2, brushSize*2);
+            var grd = context1.createRadialGradient(x, y, brushSize*2/ brushGrad,x-brushSize,y-brushSize,brushSize*2);
             grd.addColorStop(0, brushColour);
             grd.addColorStop(1, 'rgba(255,255,255,0)')
-            context.fillStyle = brushColour;
-            context.fill();
+            context1.fillStyle = brushColour;
+            context1.fill();
         }
     }
 }
@@ -74,7 +87,7 @@ function colourSet(input) {
     document.getElementById('colourpicker').style.backgroundColor = input;
 }
 function reset() {
-    context.clearRect(0, 0, canvas.width, canvas.height);
+    context1.clearRect(0, 0, canvas1.width, canvas1.height);
 }
 function erase() {
     mode = "erase";
@@ -83,10 +96,10 @@ function brushSet(input) {
     brushSize = input;
 }
 function backgroundColour(input) {
-    context2.beginPath();
-    context2.rect(0, 0, canvas2.width, canvas2.height);
-    context2.fillStyle = input;
-    context2.fill();
+    context.beginPath();
+    context.rect(0, 0, canvas.width, canvas.height);
+    context.fillStyle = input;
+    context.fill();
     back = input;
 }
 window.onmousemove = function (e) {
@@ -95,8 +108,8 @@ window.onmousemove = function (e) {
     posit[1] = e.clientY || e.pageY;
     if (mouse === true) {
         if (mode === "erase") {
-            if (posit[1] > canvas.height || posit[0] > canvas.width) { }
-            else {context.clearRect(posit[0] - brushSize, posit[1] - brushSize, brushSize*2, brushSize*2);}
+            if (posit[1] > canvas1.height || posit[0] > canvas1.width) { }
+            else {context1.clearRect(posit[0] - brushSize, posit[1] - brushSize, brushSize*2, brushSize*2);}
         }
         else {
             draw(posit[0], posit[1]);
@@ -106,24 +119,12 @@ window.onmousemove = function (e) {
 window.onmousedown = function (e) {
     mouse = true;
     if (mode === "erase") {
-        context.clearRect(posit[0] - brushSize, posit[1] - brushSize, brushSize * 2, brushSize * 2);
+        context1.clearRect(posit[0] - brushSize, posit[1] - brushSize, brushSize * 2, brushSize * 2);
     }
     else {
         draw(posit[0], posit[1]);
     }
 };
 window.onmouseup = function (e) {
-    mouse = false;
-};
-window.touchstart = function (e) {
-    mouse = true;
-    if (mode === "erase") {
-        context.clearRect(posit[0] - brushSize, posit[1] - brushSize, brushSize * 2, brushSize * 2);
-    }
-    else {
-        draw(posit[0], posit[1]);
-    }
-}
-window.touchend = function (e) {
     mouse = false;
 };
